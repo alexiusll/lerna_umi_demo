51f61e781474551857bbaa71db0bfeee6691c063
Alexiusll <linkenzone@163.com>
2021-07-06 14:11:11 +0800

    更新

--- packages/MyButton/rollup.config.js
+++ packages/MyButton/rollup.config.js
@@ -4,45 +4,43 @@
  * @Descripttion: Do not edit
  */
 
-import typescript from "rollup-plugin-typescript2";
+import typescript from 'rollup-plugin-typescript2';
 // import commonjs from "rollup-plugin-commonjs";
 // import resolve from "rollup-plugin-node-resolve";
-import postcss from "rollup-plugin-postcss";
+import postcss from 'rollup-plugin-postcss';
 
 // const override = { compilerOptions: { declaration: false } };
 
 export default [
   {
-    input: "./src/index.tsx",
+    preserveModules: true,
+    input: './src/index.tsx',
     output: {
-      file: "dist/index.js",
-      format: "esm",
-      exports: "named",
+      dir: './dist',
+      // file: 'dist/index.js',
+      format: 'esm',
+      exports: 'named',
       sourcemap: true,
       // 添加globals
       globals: {
-        react: "React",
-        "react-dom": "ReactDOM",
+        react: 'React',
+        'react-dom': 'ReactDOM',
       },
     },
     // 添加externs
     // 告诉rollup不要将react打包，而作为外部依赖
-    external: ["react", "react-dom"],
+    external: ['react', 'react-dom'],
     plugins: [
-      // postcss处理less文件，并用 autoprefixer 处理兼容,cssnano 压缩
       postcss({
-        minimize: true,
         modules: true,
-        use: {
-          sass: null,
-          stylus: null,
-          less: { javascriptEnabled: true },
-        },
-        extract: true,
+        minimize: true,
+        extensions: ['.less', '.css'],
+        use: ['less'],
+        extract: 'style.css', // 输出路径
       }),
       ,
       typescript({
-        tsconfig: "tsconfig.json",
+        tsconfig: 'tsconfig.json',
         useTsconfigDeclarationDir: true,
       }),
     ],
